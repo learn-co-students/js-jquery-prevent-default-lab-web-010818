627e04be9ea5eb996e9cd289d5403d6a632d83f0
morgannegagne <morgannegagne@gmail.com>
2018-02-28 08:28:33 -0500

    Done.

--- js/todo.js
+++ js/todo.js
@@ -1,5 +1,17 @@
 $(document).ready(function(){
   // call functions here
+  submitForm();
 });
 
 // define functions here
+
+function submitForm(){
+  $('form').on('submit', function(event){
+    event.preventDefault()
+    let item = document.createElement('p')
+    let itemValue = $('#item').val();
+    item.innerHTML = `<li> ${itemValue} </li>`
+    $('ol').append(item)
+    $('#item').val("");
+  })
+}
